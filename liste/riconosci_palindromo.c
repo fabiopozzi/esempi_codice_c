3a614c8c089c229f5cd4435dda086395e06fb513
Fabio Pozzi <pozzi.fabio@gmail.com>
2020-11-08 22:19:12 +0100

    Aggiunti testi esercizi su liste e code.

---
+++ liste/riconosci_palindromo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+/*
+** Una parola o una frase palindroma può essere letta indifferentemente da sinistra
+** a destra o da destra a sinistra.
+** Scrivere una funzione che controlli se una stringa è palindroma usando una pila.
+ */
