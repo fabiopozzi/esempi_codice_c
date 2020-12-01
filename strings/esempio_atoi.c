6a5053434206f394609a366f6d865e5de61ea722
Fabio Pozzi <pozzi.fabio@gmail.com>
2020-12-01 18:27:57 +0100

    Esempio uso atoi

---
+++ strings/esempio_atoi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int val;
+    char str[20];
+
+    strcpy(str, "98993489");
+    val = atoi(str);
+    printf("%s= %d\n", str, val);
+
+    return (0);
+}
