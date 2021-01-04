cfa6c09e2dcabe255faf71f8de8646315bd5136b
Fabio Pozzi <pozzi.fabio@gmail.com>
2021-01-04 15:08:26 +0100

    modifiche esempi stringhe

--- strings/esempio_atoi.c
+++ strings/esempio_atoi.c
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define N 4
 int main()
 {
-    int val;
-    char str[20];
-
-    strcpy(str, "98993489");
-    val = atoi(str);
-    printf("%s= %d\n", str, val);
+    long val;
+    int lung = 4;
+    char variabile_a_caso[80] = "89999";
+    // prova con "9891234", "9898aaaa"   "9899   "
+    val = atol(variabile_a_caso);
+    // come faccio a sapere se c'è stato un errore??
+    printf("%s = %ld\n", variabile_a_caso, val);
 
     return (0);
 }
