eaddbef40f783662874f10af0a55e54a15ef41f3
Fabio Pozzi <pozzi.fabio@gmail.com>
2021-01-04 15:07:36 +0100

    Esempi ed esercizi su selezione e switch

---
+++ selezione/es_switch3.c
@@ -0,0 +1,7 @@
+/*
+Su una linea ferroviaria, rispetto alla tariffa piena, gli utenti pensionati
+usufruiscono di uno sconto del 10%, gli studenti del 15% e i disoccupati del
+25%. Codificando i pensionati con una P, gli studenti con una S e i disoccupati
+con una D, scrivere un programma che, richiesto il costo di un biglietto e
+l'eventuale condizione particolare dell'utente, visualizzi l'importo da pagare.
+*/
---
+++ selezione/es_switch4.c
@@ -0,0 +1,14 @@
+/*
+Scrivere un programma che visualizzi il seguente menu: MENU DI PROVA - premere:
+            a) Per immettere dati
+            b) Per determinare il maggiore
+            c) Per determinare il minore
+            d) Per ordinare
+            e) Per visualizzare
+Scelta: _
+attenda l'immissione di un carattere da parte dell'utente e infine, usando il
+costrutto switch, visualizzi una scritta corrispondente alla scelta effettuata
+(per esempio del tipo: In esecuzione l'opzione a). Se la scelta non è tra quelle
+proposte (a,b,c,d,e) deve invece essere visualizzata la scritta Opzione
+inesistente.
+*/
