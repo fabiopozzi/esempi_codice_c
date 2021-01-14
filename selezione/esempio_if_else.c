f094eed96140bda9af881185f76e31870dd3dd3a
Fabio Pozzi <pozzi.fabio@gmail.com>
2021-01-14 16:44:03 +0100

    Aggiunto esempio if - else

---
+++ selezione/esempio_if_else.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int n = 10;
+    // se il numero e' positivo stampo "POSITIVO"
+    if (n > 0) {
+        printf("POSITIVO");
+    }
+    printf("ARRIVEDERCI!");
+
+    // cosa succede se n fosse == -20?
+    return 0;
+}
