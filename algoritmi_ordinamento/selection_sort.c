a672e7f308de4e54284a8be01b2b3793b93291b3
Fabio Pozzi <pozzi.fabio@gmail.com>
2021-01-14 16:41:00 +0100

    Selection sort rivisto in 1 solo senso e su numeri

--- algoritmi_ordinamento/selection_sort.c
+++ algoritmi_ordinamento/selection_sort.c
@@ -1,64 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-void selection_sort(int n, char *argv[], int par);
+#define N 10
 
-int main(int argc, char *argv[])
-{
-    int par = atoi(argv[argc - 1]);
-    // 1 ordina in modo crescente
-    // 0 ordina in modo decrescente
-    selection_sort(argc - 1, argv, par);
-    return 0;
-}
-
-/* TODO: ordina delle stringhe, andrebbero convertiti in numeri interi ed
- * inseriti in un array */
-void selection_sort(int n, char *argv[], int par)
+int main()
 {
     int i, j;
-    int min;
-    char *p;
-    int cur_num, min_val;
+    int array[N] = {1, 50, 44, 3, 24, 7, 15, 42, 11, 67};
+    int posizione_minimo, valore_minimo;
+    int tmp;
 
-    for (i = 1; i < n - 1; i++) {
-        min = i;
-        min_val = atoi(argv[i]);
-        for (j = i + 1; j < n; j++) {
-          cur_num = atoi(argv[j]);
-          if (par == 0) {
-            // trova posizione stringa minima
-            if (cur_num < min_val) {
-              min = j;
-              min_val = atoi(argv[min]);
+    for (i = 0; i < N - 1; i++) { // ripeti n-1 volte
+      // il primo elemento non ordinato è in posizione i
+        // inizializza minimo
+        posizione_minimo = i; // posizione
+        valore_minimo = array[i]; // valore
+
+        // trova minimo nel resto dell'array
+        for (j = i + 1; j < N; j++) {
+
+            // trova posizione minimo
+            if (array[j] < valore_minimo) {
+              posizione_minimo = j;
+              valore_minimo = array[j];
             }
-          } else {
-            // trova posizione stringa massimo
-            if (cur_num > min_val) {
-              min = j;
-              min_val = atoi(argv[min]);
-            }
-          }
-        }
-        // scambia minimo con posizione iesima
-        p = argv[min];
-        argv[min] = argv[i];
-        argv[i] = p;
+        } // finito di trovare il minimo
+
+
+        // scambia minimo (in posizione min) con elemento in posizione i
+        tmp = array[posizione_minimo];
+        array[posizione_minimo] = array[i];
+        array[i] = tmp;
     }
-    for (i = 1; i < n; i++)
-      printf("%s\n", argv[i]);
-    return;
-}
 
-/* void bubble_sort(int n, char *argv[]) */
-/* { */
-/*     for(i=1;i<n-1;i++){ */
-/*         if(strcmp(argv[i], argv[i+1])>0) { */
-/*             p = argv[i+1]; */
-/*             argv[i+1] = argv[i]; */
-/*             argv[i] = p; */
-/*             //scambia */
-/*         } */
-/*     } */
-/* } */
+    /* stampa array ordinato */
+    for (i = 0; i < N; i++) {
+      printf("%d\t", array[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
