10299fee40e3402746d130ac3cc65a73aaae6c9c
Fabio Pozzi <pozzi.fabio@gmail.com>
2021-01-22 22:17:54 +0100

    Aggiungi rilevazione timing algoritmo

--- algoritmi_ordinamento/selection_sort.c
+++ algoritmi_ordinamento/selection_sort.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define N 10
 
 int main()
 {
+    clock_t start_t, end_t;
+    float total_t;
     int i, j;
     int array[N] = {1, 50, 44, 3, 24, 7, 15, 42, 11, 67};
     int posizione_minimo, valore_minimo;
     int tmp;
+
+    start_t = clock();
 
     for (i = 0; i < N - 1; i++) { // ripeti n-1 volte
       // il primo elemento non ordinato è in posizione i
@@ -33,11 +38,16 @@ int main()
         array[i] = tmp;
     }
 
+    end_t = clock();
+
     /* stampa array ordinato */
     for (i = 0; i < N; i++) {
       printf("%d\t", array[i]);
     }
     printf("\n");
+
+    total_t = ((float)(end_t - start_t)) / CLOCKS_PER_SEC;
+    printf("Tempo totale di elaborazione: %f\n", total_t );
 
     return 0;
 }
