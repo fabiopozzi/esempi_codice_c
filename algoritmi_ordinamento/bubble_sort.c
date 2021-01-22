acd02c0bcab177fe4f15f8072543902a566aa0a8
Fabio Pozzi <pozzi.fabio@gmail.com>
2021-01-22 22:18:19 +0100

    Aggiungi semplice esempio bubble sort

---
+++ algoritmi_ordinamento/bubble_sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define N 10
+
+int main()
+{
+    int i, j, swap;
+    int array[N] = {1, 50, 44, 3, 24, 7, 15, 42, 11, 67};
+
+    for (i = 0 ; i < N - 1; i++)
+    {
+        for (j = 0 ; j < N - i - 1; j++)
+        {
+            // se voglio ordinamento decrescente userò '<'
+            if (array[j] > array[j+1])
+            {
+                // SCAMBIO ELEMENTI
+                swap       = array[j];
+                array[j]   = array[j+1];
+                array[j+1] = swap;
+            }
+        }
+    }
+
+    printf("Array ordinato con ordine crescente:\n");
+
+    for (i = 0; i < N; i++) {
+        printf("%d\n", array[i]);
+    }
+
+    return 0;
+}
