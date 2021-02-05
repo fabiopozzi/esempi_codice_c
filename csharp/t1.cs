d0dc0d6da7e89cb69a3299c234a3b913b5556994
Fabio Pozzi <fabio@fabiopozzi.net>
2021-02-05 07:42:06 +0100

    Aggiungi prog. esempio in c#

---
+++ csharp/t1.cs
@@ -0,0 +1,12 @@
+using System;
+//using System.Diagnostics;
+//using System.Threading;
+
+class esthread {
+    static void Main(string[] args)
+    {
+        //Stopwatch watch = new Stopwatch();
+
+        Console.WriteLine("algoritmo parallelo");
+    }
+}
