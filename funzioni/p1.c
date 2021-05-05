7424c9f1c14bc1c1fbfb04120b26afb058549b24
Fabio Pozzi <fabio@fabiopozzi.net>
2021-05-05 07:41:05 +0200

    Aggiunti nuovi esempi sulle funzioni

---
+++ funzioni/p1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("AAAA\n");
+    return 10;
+}
---
+++ funzioni/scambia.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// SCRIVI UNA FUNZIONE CHE SCAMBI IL VALORE DI DUE VARIABILI
+void scambia(int *x, int *y)
+{
+	int tmp;
+	// COME LI SCAMBIO?
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+
+}
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+	printf("a: %d b: %d\n", a, b); // 10 20
+	scambia(&a, &b);
+	printf("a: %d b: %d\n", a, b); // 20 10
+
+	return 0;
+}
+
