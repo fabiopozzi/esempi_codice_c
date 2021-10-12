52834413f1539bac6d2e9f47ec12a357321fdfcc
Fabio Pozzi <fabio@fabiopozzi.net>
2021-10-12 18:31:17 +0200

    Agg. altro esempio uso colori con ANSI-color-codes

---
+++ colore/colore2.c
@@ -0,0 +1,10 @@
+#include "ANSI-color-codes.h"
+#include <stdio.h>
+
+int main(){
+	int a;
+	printf(BRED "GRASSETTO rosso\n");
+	scanf("%d", &a);
+	// NB: nel caso del sottolineato anche lo spazio viene sottolineato ed e' colorato
+return 0;
+};
